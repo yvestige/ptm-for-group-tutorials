f8e4ec63b60710ab6f06b7523dca6ae7a4f63929
yvbal <70612985+Yves242@users.noreply.github.com>
2024-04-07 19:20:50 +0800

    updated files for arrays and strings

--- Presentations/CMSC 21: Arrays and Stringts/find-green-folder-nc.c
+++ Presentations/CMSC 21: Arrays and Stringts/find-green-folder-nc.c
@@ -3,26 +3,22 @@
 void main() {
 
     // for the purpose of example, declare variables
+    int empty = 0;
     int box = 1;
     int bottle = 2;
     int green_folders = 3;
 
     // this contains all the items
-    int container[7] = {0, box, 0, 0, bottle, green_folders, 0};
+    int container[7] = {0, box, 0, 0, bottle, 0, 0};
     
     // acquired item will go here
     int acquired_item;
 
     // go through each of the items
     for (int i=0; i<7; i++) {
-
-        // check if current item is the green folders
         if (container[i] == green_folders) {
-
-            // if it is, put value to variable 'acquired_item'
             acquired_item = container[i];
-
-            // stop the current iteration
+            container[i] = empty;   // 0
             break;
         }
     }
